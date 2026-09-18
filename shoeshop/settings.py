939f0417b458823024bd3ef2cd088a593b7f59e1
userAlex1 <mwendaalexis@gmail.com>
2026-09-18 22:59:06 +0300

    Configure Neon PostgreSQL

--- shoeshop/settings.py
+++ shoeshop/settings.py
@@ -4,6 +4,8 @@ Django settings for shoeshop project.
 import os
 from pathlib import Path
 from decouple import config
+from pathlib import Path
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -65,11 +67,17 @@ WSGI_APPLICATION = 'shoeshop.wsgi.application'
 # Defaults to SQLite for easy local dev. Swap to Postgres in production
 # by setting DATABASE_URL and using dj-database-url (see README).
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+        conn_max_age=600,
+    )
 }
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
